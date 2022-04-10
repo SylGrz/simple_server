79b87306d57e662890e6d032a116da97889d0b47
Sylwia Grzelak <103308904+SylGrz@users.noreply.github.com>
2022-04-10 09:46:44 +0200

    modified response

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Sylwia chce na wakacje');
+  res.end('Sylwia chce na długie wakacje');
 }
 
 const server = http.createServer(requestListener);
